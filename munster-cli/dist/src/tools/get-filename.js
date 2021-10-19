ca2d0cad36434432931e13341460275f82d76982
darius <dariusa.bualanjr@pccw.com>
2021-10-20 06:41:05 +0800

    removed git cache

--- munster-cli/dist/src/tools/get-filename.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFilename = void 0;
-function getFilename(filepath) {
-    var pathArr = filepath.split('/');
-    return pathArr[pathArr.length - 1];
-}
-exports.getFilename = getFilename;
--- munster-cli/dist/src/tools/kebab-to-camel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.kebabToCamel = void 0;
-function kebabToCamel(str) {
-    return str.replace(/-./g, function (x) { return x.toUpperCase()[1]; });
-}
-exports.kebabToCamel = kebabToCamel;
